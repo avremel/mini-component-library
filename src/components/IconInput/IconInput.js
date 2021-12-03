e15dc886558869f568db4b4d07cfe914fcd8a574
Avraham Kamenetzky <avraham@ajmadison.com>
2021-12-02 23:04:01 -0500

    icon input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,17 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES_BY_SIZE = {
+  small: {
+    '--font-size': '14px',
+    '--border-bottom': '1px solid black',
+  },
+  large: {
+    '--font-size': '18px',
+    '--border-bottom': '2px solid black',
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +24,74 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Label>{label}</Label>
+      <InputWrapper>
+        <Input
+          placeholder={placeholder}
+          width={width}
+          style={STYLES_BY_SIZE[size]}
+        />
+        <IconWrapper size={size}>
+          <Icon id={icon} size={size === 'large' ? '20' : '15'} />
+        </IconWrapper>
+      </InputWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 30px;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  text-align: right;
+  width: 100px;
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+`;
+
+const Input = styled.input`
+  all: unset;
+  width: ${p => p.width ? p.width + 'px' : '100%'};
+  font-size: var(--font-size);
+  border-bottom: var(--border-bottom);
+  padding-left: 25px;
+  padding-bottom: 5px;
+  color: ${COLORS.gray700};
+  font-weight: bold;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: initial;
+  }
+
+  &:focus {
+    outline: 2px solid ${COLORS.primary};
+    outline-offset: 3px;
+  }
+
+  &:hover {
+    color: ${COLORS.black}
+  }
+`;
+
+const IconWrapper = styled.div`
+  pointer-events: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  color: ${COLORS.gray500};
+
+  ${Input}:hover + & {
+    color: ${COLORS.black}
+  }
+`;
 
 export default IconInput;
